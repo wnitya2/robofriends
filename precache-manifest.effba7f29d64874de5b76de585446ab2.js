d07b44b377c92a594612d3cc0f36c3c9b90b7974
wijayanti.nitya <wijayanti.nitya@gmail.com>
2019-04-03 11:10:19 +0700

    Updates

---
+++ precache-manifest.effba7f29d64874de5b76de585446ab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "dc2372f9442d468114ef",
+    "url": "/robofriends/static/js/main.dc2372f9.chunk.js"
+  },
+  {
+    "revision": "c0adf201629407e743cd",
+    "url": "/robofriends/static/js/2.c0adf201.chunk.js"
+  },
+  {
+    "revision": "dc2372f9442d468114ef",
+    "url": "/robofriends/static/css/main.4c795ddf.chunk.css"
+  },
+  {
+    "revision": "c0adf201629407e743cd",
+    "url": "/robofriends/static/css/2.46b1c7e3.chunk.css"
+  },
+  {
+    "revision": "b6b60de84eacb2aaa8615afc5f82a402",
+    "url": "/robofriends/index.html"
+  }
+];
