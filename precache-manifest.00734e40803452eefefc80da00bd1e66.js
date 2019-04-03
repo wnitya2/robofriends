d6cbaafde3add12c04aa2584cce8e9bdc18a8ae6
wijayanti.nitya <wijayanti.nitya@gmail.com>
2019-04-03 10:36:56 +0700

    Updates

---
+++ precache-manifest.00734e40803452eefefc80da00bd1e66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d82bfd4ecb53450f9a5d",
+    "url": "/static/js/main.d82bfd4e.chunk.js"
+  },
+  {
+    "revision": "b1cfe0b2e0fc9aa413eb",
+    "url": "/static/js/2.b1cfe0b2.chunk.js"
+  },
+  {
+    "revision": "d82bfd4ecb53450f9a5d",
+    "url": "/static/css/main.909aed29.chunk.css"
+  },
+  {
+    "revision": "b1cfe0b2e0fc9aa413eb",
+    "url": "/static/css/2.46b1c7e3.chunk.css"
+  },
+  {
+    "revision": "7fbc74d4fdb0a5783ced6b63e789d072",
+    "url": "/index.html"
+  }
+];
